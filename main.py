5a6fedbaf6e9fb2a44cbfafc81ad4f537052530e
Harshita Guduru <guduruharshita28@gmail.com>
2025-12-24 13:00:00 +0000

    Add skip functionality and rule label display

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 from game.ui import TypingGame
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     root = tk.Tk()
-    root.minsize(480, 430)
+    root.minsize(500, 450)
     TypingGame(root)
     root.mainloop()
